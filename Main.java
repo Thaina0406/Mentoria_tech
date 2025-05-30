fb69d9067d058fa1e2f771c183fe9c19c631e266
Thaina0406 <thaina.ribeiro.2009@aluno.edu.pi.gov.br>
2025-05-30 13:11:00 -0300

    Avaliação

--- Main.java
+++ Main.java
@@ -1,17 +1,41 @@
 import java.util.Scanner;
 
-public class Main {
+class aula2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite a temperatura em Celsius: ");
-        double celsius = scanner.nextDouble();
+        System.out.println("Coloque as notas:");
+        System.out.print("Nota 1: ");
+        double n1 = scanner.nextDouble();
+        System.out.print("Nota 2: ");
+        double n2 = scanner.nextDouble();
+        System.out.print("Nota 3: ");
+        double n3 = scanner.nextDouble();
+        System.out.print("Nota 4: ");
+        double n4 = scanner.nextDouble();
+        System.out.print("Nota 5: ");
+        double n5 = scanner.nextDouble();
+        System.out.print("Nota 6: ");
+        double n6 = scanner.nextDouble();
+        System.out.print("Nota 7: ");
+        double n7 = scanner.nextDouble();
+        System.out.print("Nota 8: ");
+        double n8 = scanner.nextDouble();
 
-        double fahrenheit = (celsius * 9 / 5) + 32;
-        double kelvin = celsius + 273.15;
-        System.out.printf("Temperatura em Celsius: %.2f°C\n", celsius);
-        System.out.printf("Temperatura em Fahrenheit: %.2f°F\n", fahrenheit);
-        System.out.printf("Temperatura em Kelvin: %.2fK\n", kelvin);
+        double Ps = (n1 + n2) / 2;
+        double Sb = (n3 + n4) / 2;
+        double Tb = (n5 + n6) / 2;
+        double Ss = (n7 + n8) / 2;
+        double MS = (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8) / 8;
+
+        System.out.printf("Média do 1º Bimestre: %.2f%n", Ps);
+        System.out.printf("Média do 2º Bimestre: %.2f%n", Sb);
+        System.out.printf("Média do 3º Bimestre: %.2f%n", Tb);
+        System.out.printf("Média do 4º Bimestre: %.2f%n", Ss);
+        System.out.printf("Média Final: %.2f%n", MS);
+
         scanner.close();
     }
 }
+
+
